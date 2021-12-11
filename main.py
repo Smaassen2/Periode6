031a3183cb238a1723ea2ba2f72062c21bee0dd3
Smaassen2 <71068315+Smaassen2@users.noreply.github.com>
2021-12-11 14:07:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,70 @@
 import scipy.stats
+
+
 def readfile():
-    # File inlezen
-    file = open("ResultsOfMarkersAndAnthocyninConcentration.txt","r")
-    reformed_string = ""
-    #Om een newline achter de laatste concentratie weg te halen
+    sig_markerlist = []
+    sig_tstatisticslist = []
+    sig_pvaluelist = []
+    notsig_markerlist = []
+    notsig_tstatisticslist = []
+    notsig_pvaluelist = []
+
+    file = open("ResultsOfMarkersAndAnthocyninConcentration.txt", "r")
     line = file.readline().strip()
-    # Alle concentraties in een lijst zetten
     concentrationlist = line.split("\t")
-    markerlist = []
-    tstatisticslist = []
-    pvaluelist = []
+
     for line in file:
         if line != " ":
             lista = []
             listb = []
             index = 0
-
             line = file.readline().strip()
             bandpresencelist = line.split("\t")
             marker = bandpresencelist[0]
             del bandpresencelist[0]
             for bandpresence in bandpresencelist:
                 if bandpresence == "a":
-                    lista.append(float(concentrationlist[index].replace(",", ".")))
+                    lista.append(float(concentrationlist[index].replace
+                                       (",", ".")))
                 elif bandpresence == "b":
-                    listb.append(float(concentrationlist[index].replace(",", ".")))
+                    listb.append(float(concentrationlist[index].replace
+                                       (",", ".")))
                 index += 1
 
-            tstatistic, pvalue = scipy.stats.ttest_ind(listb, lista, equal_var=True)
-            if pvalue < 0.1:
-                markerlist.append(marker)
-                tstatisticslist.append(tstatistic)
-                pvaluelist.append(pvalue)
+            tstatistic, pvalue = scipy.stats.ttest_ind(listb, lista,
+                                                       equal_var=True)
+            tstatistic = str(tstatistic)
+            if pvalue <= 0.05:
+                pvalue = str(pvalue)
+                tstatistic = tstatistic.replace(".", ",")
+                pvalue = pvalue.replace(".", ",")
+                sig_markerlist.append(marker)
+                sig_tstatisticslist.append(tstatistic)
+                sig_pvaluelist.append(pvalue)
+            else:
+                pvalue = str(pvalue)
+                tstatistic = tstatistic.replace(".", ",")
+                pvalue = pvalue.replace(".", ",")
+                notsig_markerlist.append(marker)
+                notsig_tstatisticslist.append(tstatistic)
+                notsig_pvaluelist.append(pvalue)
 
-    output = open("Significant.txt", "w")
-    index2 = 1
-    for marker in markerlist:
-        output.write(marker)
-        if index2 != len(markerlist):
-            output.write(", ")
-            index2 += 1
+    output = open("Results.txt", "w")
+    index2 = 0
+    index3 = 0
+    output.write("Marker" + "\t" + "p-value" + "\t" + "t-statistic" +
+                 "\n")
+    for marker in sig_markerlist:
+        output.write(marker + "\t" + sig_pvaluelist[index2] + "\t" +
+                     str(sig_tstatisticslist[index2]) + "\n")
+        index2 += 1
     output.write("\n")
-    index2 = 1
-    for tstatisticpercentage in tstatisticslist:
-        output.write(str(tstatisticpercentage))
-        if index2 != len(markerlist):
-            output.write(", ")
-            index2 += 1
-    output.write("\n")
-    index2 = 1
-    for pvaluepercentage in pvaluelist:
-        output.write(str(pvaluepercentage))
-        if index2 != len(markerlist):
-            output.write(", ")
-            index2 += 1
+    for marker in notsig_markerlist:
+        output.write(marker + "\t" + notsig_pvaluelist[index3] + "\t" +
+                     str(notsig_tstatisticslist[index3]) + "\n")
+        index3 += 1
     output.close()
 
 
 if __name__ == '__main__':
     readfile()
-
